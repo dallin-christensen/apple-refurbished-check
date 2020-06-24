349ba736dc8bebcc1fd27b261d64566f5f62287b
Dallin Christensen <christensen.dallin@gmail.com>
2020-06-23 18:16:40 -0700

    friggin work on a raspberry pi ya a-hole

--- index.js
+++ index.js
@@ -30,9 +30,7 @@ const items = [
 ]
 
 const scrapeAppleRefurbAvailability = async ({ url }) => {
-  // const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
-  // const browser = await puppeteer.launch({headless: false});
-  const browser = await puppeteer.launch({headless: true});
+  const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
   const page = await browser.newPage();
 
   await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36');
